eb6ce3aea00c47025f93fdee873a43489cf5b864
keitheous <keithchongwy@gmail.com>
2018-02-01 22:19:27 +0800

    introduction to directory and angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { InstallationComponent } from './installation/installation.component';
+import { DirectoriesComponent } from './directories/directories.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    InstallationComponent
+    InstallationComponent,
+    DirectoriesComponent
   ],
   imports: [
     BrowserModule,
